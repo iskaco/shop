6235cf9662659833c6a9c4403deeba5bac609df8
mhmalekian <malekian610@gmail.com>
2025-04-02 12:03:49 +0330

    fix product store request

--- app/Http/Requests/Admins/Products/ProductBaseRequest.php
+++ app/Http/Requests/Admins/Products/ProductBaseRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Admins\Products;
 
 use App\Http\Requests\Admins\AdminsAuthRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
 
 abstract class ProductBaseRequest extends AdminsAuthRequest
 {
@@ -30,12 +31,32 @@ abstract class ProductBaseRequest extends AdminsAuthRequest
 
     public function prepareForValidation()
     {
+        $data = [
+            'name_en' => $this->name_en,
+            'name_ar' => $this->name_ar,
+            'description_en' => $this->description_en,
+            'description_ar' => $this->description_ar,
+            'short_description_en' => $this->short_description_en,
+            'short_description_ar' => $this->short_description_ar,
+        ];
+
+        $validator = Validator::make($data, [
+            'name_en' => ['required', 'string', 'max:255'],
+            'name_ar' => ['required', 'string', 'max:255'],
+            'description_en' => ['nullable', 'string'],
+            'description_ar' => ['nullable', 'string'],
+            'short_description_en' => ['nullable', 'string'],
+            'short_description_ar' => ['nullable', 'string'],
+        ]);
+
+        $validator->validate();
+
         $this->merge([
             'name' => ['en' => $this->name_en, 'ar' => $this->name_ar],
             'description' => ['en' => $this->description_en, 'ar' => $this->description_ar],
             'short_description' => ['en' => $this->short_description_en, 'ar' => $this->short_description_ar],
-            'category_id' => $this->category_id['id'],
-            'brand_id' => $this->brand_id['id'],
+            'category_id' => $this->category ? $this->category_id['id'] : null,
+            'brand_id' => $this->brand ? $this->brand_id['id'] : null,
         ]);
     }
 }
