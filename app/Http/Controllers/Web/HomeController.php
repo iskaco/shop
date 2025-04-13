061845fc9c8dfb76fb7d8d2e1284705779e1676c
mhmalekian <malekian610@gmail.com>
2025-04-13 18:15:37 +0330

    add category products action for list category products

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Web;
 use App\Actions\Categories\FeaturedCategoryIndex;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryResource;
+use App\Http\Resources\ProductResource;
+use App\Models\Category;
 use Inertia\Inertia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
@@ -13,9 +15,11 @@ class HomeController extends Controller
     public function home()
     {
         $categories = app(FeaturedCategoryIndex::class)->execute();
+        $random_products = $this->randomProduct();
 
         return Inertia::render('Welcome', [
             'categories' => CategoryResource::collection($categories),
+            'random_products' => ProductResource::collection($random_products),
         ]);
     }
 
@@ -26,5 +30,14 @@ class HomeController extends Controller
         } catch (\Throwable $th) {
             // throw $th;
         }
+    }
+
+    public function randomProduct()
+    {
+        $selected_category_slug = 'electronics'; // TODO
+        $category = Category::where('slug', $selected_category_slug)->first() ?? Category::inRandomOrder()->first();
+        $product = $category->products()->inRandomOrder()->take(4)->get();
+
+        return $product;
     }
 }
